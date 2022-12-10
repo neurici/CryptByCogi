bad7250801bd7d4789117e126be7b46cf90c268a
neurici <neurici@gmail.com>
2022-12-10 18:26:11 +0200

    Add files via upload

---
+++ ScryptByCogi.py
@@ -0,0 +1,164 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+import sys
+import funcy
+import base64
+import requests
+import Crypto.Protocol
+from Crypto import Random
+from colorama import init
+from Crypto.Cipher import AES
+from colorama import Fore, Style
+
+version = "1.0"
+
+init()
+
+banner = r'''
+============================================================================
+|   _____                      _     ____            _____                  |
+|  / ____|                    | |   |  _ \          / ____|             (_) |
+| | |      _ __  _   _  _ __  | |_  | |_) | _   _  | |      ___    __ _  _  |
+| | |     | '__|| | | || '_ \ | __| |  _ < | | | | | |     / _ \  / _` || | |
+| | |____ | |   | |_| || |_) || |_  | |_) || |_| | | |____| (_) || (_| || | |
+|  \_____||_|    \__, || .__/  \__| |____/  \__, |  \_____|\___/  \__, ||_| |
+|                 __/ || |                   __/ |                 __/ |    |
+|                |___/ |_|                  |___/                 |___/     |
+|                                                                           |
+|                          [Creat de Cogian Sergiu]                         |
+|                                                                           |
+|                                                                           |
+|		   Tastati 'E' pentru a cripta un fisier                    |
+|		   Tastati 'D' pentru a decripta un fisier                  |
+|		   Tastati 'I' pentru a inchide aplicatia                   |
+=============================================================================
+'''
+print(Fore.LIGHTBLUE_EX + banner)
+print(Style.RESET_ALL)
+
+def update():
+    print("Checking for updates...")
+    Secret_Keeper = requests.get("https://raw.githubusercontent.com/Sameera-Madhushan/Secret-Keeper/master/Secret-Keeper.py").content.decode(
+        "UTF-8")
+    if version not in Secret_Keeper:
+        co = input("A new version of Secret Keeper is available. Would you like to update?[yes/no] - ").lower()
+        if co == "yes":
+            os.system('cd .. && rm -r Secret-Keeper && git clone https://github.com/Sameera-Madhushan/Secret-Keeper')
+        if co == "no":
+            print(Fore.LIGHTBLUE_EX + banner)
+            choice()
+        else:
+            digg = str(input("Ne pare rau! Optiunea selectata nu este disponibila. Doriti sa renuntati[da/nu] - ").lower())
+            if digg == "da":
+                quit()
+                if digg == "nu":
+                    print(Fore.LIGHTBLUE_EX + banner)
+                    choice()
+                else:
+                    exit()
+    else:
+        print("Secret Keeper is Upto Date.")
+        exit()
+
+def quit():
+    alpha = input("Sunteti sigur?[da/nu] - ").lower()
+    if alpha == "da":
+        exit()
+    if alpha == "nu":
+        print(Fore.LIGHTBLUE_EX + banner)
+        choice()
+
+def choice():
+    try:
+        selection = input("root@CriptServer:- ").upper()
+        if selection == "C":
+
+            usr_key = input("Introduceti o cheie care va fi utilizata ca cheie de criptare:- ")
+            salt = b'\x9aX\x10\xa6^\x1fUVu\xc0\xa2\xc8\xff\xceOV'
+            key = Crypto.Protocol.KDF.PBKDF2(password=usr_key, salt=salt, dkLen=32, count=10000)
+            iv = Random.new().read(AES.block_size)
+            bs = AES.block_size
+
+
+            def pad(s):
+                return s + (bs - len(s) % bs) * chr(bs - len(s) % bs).encode('utf-8')
+
+            def encrypt(raw):
+                raw = pad(raw.encode("utf-8"))
+                cipher = AES.new(key, AES.MODE_CBC, iv)
+                return base64.b64encode(key + iv + cipher.encrypt(raw))
+
+
+            def encryptFile(fileIn, chunksize=64*1024):
+                fileOut = fileIn + ".criptat"
+                cipher = AES.new(key, AES.MODE_CBC, iv)
+                with open(fileIn, "rb") as plain:
+                    with open(fileOut, "wb") as outFile:
+                        outFile.write(base64.b64encode(key + iv))
+
+                        while True:
+                            chunk = plain.read(chunksize)
+                            if len(chunk) == 0:
+                                break
+                            chunk = pad(chunk)
+                            outFile.write(base64.b64encode(cipher.encrypt(chunk)))
+                os.remove(fileIn)
+            encryptFile(input("Introduceti numele fisierului pe care doriti sa il criptati:- "))
+
+
+        if selection == "D":
+
+            def unpad(s):
+                return s[:-ord(s[len(s) - 1:])]
+
+            def decrypt(l):
+                l = base64.b64decode(l)
+                alpha = l[:32]
+                key == alpha
+                iv = l[32:32 + 16]
+                cipher = AES.new(key, AES.MODE_CBC, iv)
+                return unpad(cipher.decrypt(l[48:]))
+
+            def decryptFile(fileIn, chunksize=24*1024):
+                with open(fileIn, "rb") as encryptedFile:
+                    encrypted = base64.b64decode(encryptedFile.read(64))
+                    setup = encrypted[:48]
+                    key_confirm = input("Va rog sa introduceti cheia folosita pentru criptarea fisierului:- ")
+                    salt = b'\x9aX\x10\xa6^\x1fUVu\xc0\xa2\xc8\xff\xceOV'
+                    key_check = Crypto.Protocol.KDF.PBKDF2(password=key_confirm, salt=salt, dkLen=32, count=10000)
+                    if key_check == setup[:32]:
+                        print("Felicitari! Parola este corecta!")
+                    else:
+                        print("Ghinion! Parola este incorecra")
+                        sys.exit(0)
+
+                    iv = setup[32:]
+                    cipher = AES.new(key_check, AES.MODE_CBC, iv)
+                    with open(fileIn[:-7], "wb") as decryptedFile:
+                        encrypted = base64.b64decode(encryptedFile.read())
+                        chunks = list(funcy.chunks(chunksize, encrypted))
+                        for chunk in chunks:
+                            decrypted_chunk = unpad(cipher.decrypt(chunk))
+                            decryptedFile.write(decrypted_chunk)
+
+            decryptFile(input("Introduceti numele fisierului pe care doriti sa il decriptati:- "))
+
+        if selection == 'U':
+            update()
+
+        if selection == 'Q':
+            quit()
+
+    except(KeyboardInterrupt):
+        print("Program intrerupt")
+        exit
+
+choice()
+
+
+
+
+
+
+
